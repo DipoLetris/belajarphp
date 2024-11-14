debe926106c798f922b4320df4d5a899eb29b643
Dipo Letris <dipoarya420@gmail.com>
2024-11-14 23:05:34 +0700

    belajar function pada php

---
+++ latihan3.php
@@ -0,0 +1,7 @@
+<?php
+//menampilkan tanggal dengan format tertentu
+// echo date( "l, d-M-Y" );
+// echo time();
+//echo date("l", time()+60*60*24*10)
+echo date("l",  mktime(0,0,0,9,10,2004));
+?>
---
+++ latihan4.php
@@ -0,0 +1,22 @@
+<?php
+function waktuSaatIni() {
+    date_default_timezone_set('Asia/Jakarta'); // Set timezone ke Jakarta
+
+    $jam = date("H"); // Mendapatkan jam saat ini menggunakan time()
+   
+    if ($jam >= 5 && $jam < 12) {
+        return "Selamat Pagi!";
+    } elseif ($jam >= 12 && $jam < 18) {
+        return "Selamat Siang!";
+    } else {
+        return "Selamat Malam!";
+    }
+
+}
+
+function cetak($nama){
+    echo waktuSaatIni()." $nama"; 
+}
+
+cetak("Dipo");
+?>
